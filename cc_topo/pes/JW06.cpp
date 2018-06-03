174eda04476d464e012a880cfa9bdce7022f2940
dave <dave@dave-Dell-System-XPS-L322X>
2018-06-03 20:13:11 +1000

    adding temp and theta initial conditions to the jablonowski and williamson 06 test setup file

--- cc_topo/pes/JW06.cpp
+++ cc_topo/pes/JW06.cpp
@@ -31,6 +31,8 @@ using namespace std;
 #define ETA_0 0.252
 #define GRAVITY 9.80616
 #define OMEGA 7.29212e-5
+#define CP 1005.7
+#define KAPPA (RD/CP)
 
 double A[27] = {0.002194067,0.004895209,0.009882418,0.01805201,0.02983724,0.04462334,0.06160587,0.07851243,0.07731271,
                 0.07590131, 0.07424086, 0.07228744, 0.06998933,0.06728574,0.06410509,0.06036322,0.05596111,0.05078225,
@@ -86,7 +88,7 @@ double f_topog(double* x) {
 double u_init(double* x, int ki) {
     double phi     = asin(x[2]/RAD_EARTH);
     double theta   = atan2(x[1], x[0]);
-    double eta     = 0.5*(A[ki+0] + B[ki+0] + A[ki+1] + B[ki+1]);
+    double eta     = 0.5*(A[ki+0] + B[ki+0] + A[ki+1] + B[ki+1]);//TODO: check this, A abd B arrays go down not up
     double eta_v   = 0.5*(eta - ETA_0)*M_PI;
     double us      = U0*pow(cos(eta_v), 1.5)*sin(2.0*phi)*sin(2.0*phi);
     double theta_c = 1.0*M_PI/9.0;
@@ -103,7 +105,7 @@ double v_init(double* x, int ki) {
 
 double t_init(double* x, int ki) {
     double phi     = asin(x[2]/RAD_EARTH);
-    double eta     = 0.5*(A[ki+0] + B[ki+0] + A[ki+1] + B[ki+1]);
+    double eta     = 0.5*(A[ki+0] + B[ki+0] + A[ki+1] + B[ki+1]);//TODO: check this, A abd B arrays go down not up
     double eta_v   = 0.5*(eta - ETA_0)*M_PI;
     double t_avg   = t_bar(eta);
     double a       = 10.0/63.0 - 2.0*pow(sin(phi), 6.0)*(cos(phi)*cos(phi) + 1.0/3.0);
@@ -113,8 +115,53 @@ double t_init(double* x, int ki) {
     return temp;
 }
 
+double rho_init(double* x, int ki) {
+    double tb  = t_init(x, ki    );
+    double tt  = t_init(x, ki + 1);
+    double th  = 0.5*(tb + tt);
+    double pb  = A[ki+0]*P0 + B[ki+0]*P0;//TODO: check this, A abd B arrays go down not up
+    double pt  = A[ki+1]*P0 + B[ki+1]*P0;//TODO: check this, A abd B arrays go down not up
+    double ph  = 0.5*(pb + pt);
+    double rho = ph/RD/th;
+
+    return rho;
+}
+
+double theta_init(double* x, int ki) {
+    double phi   = asin(x[2]/RAD_EARTH);
+    double Ac    = 10.0/63.0 - 2.0*pow(phi, 6.0)*(cos(phi)*cos(phi) + 1.0/3.0);
+    double Bc    = RAD_EARTH*OMEGA*(1.6*pow(phi, 3.0)*(sin(phi)*sin(phi) + 2.0/3.0) - 0.25*M_PI);
+    double temp  = t_init(x, ki);
+    double eta   = 0.5*(A[ki+0] + B[ki+0] + A[ki+1] + B[ki+1]);//TODO: check this, A abd B arrays go down not up
+    double eta_v = 0.5*(eta - ETA_0)*M_PI;
+    double pres  = eta*P0;
+    double theta = (temp + 0.75*eta*M_PI*U0/RD*sin(eta_v)*sqrt(cos(eta_v))*(2.0*U0*Ac*pow(cos(eta_v), 1.5) + Bc))/pres;
+
+    return theta;
+}
+
+double rt_init(double* x, int ki) {
+    double theta = theta_init(x, ki);
+    double rho   = rho_init(x, ki);
+
+    return rho*theta;
+}
+
+void LoadVecs(Vec* vecs, int nk, char* fieldname, int step) {
+    int ki;
+    char filename[100];
+    PetscViewer viewer;
+
+    for(ki = 0; ki < NK; ki++) {
+        sprintf(filename, "output/%s_%.4u_%.4u.vec", fieldname, ki, step);
+        PetscViewerBinaryOpen(PETSC_COMM_WORLD, fieldname, FILE_MODE_READ, &viewer);
+        VecLoad(vecs[ki], viewer);
+        PetscViewerDestroy(&viewer);
+    }
+}
+
 int main(int argc, char** argv) {
-    int size, rank, step, ii, ki;
+    int size, rank, step, ii, ki, n2;
     static char help[] = "petsc";
     double dt = 120.0;
     double vort_0, mass_0, ener_0;
@@ -130,7 +177,6 @@ int main(int argc, char** argv) {
     SWEqn* sw;
     PrimEqns* pe;
     Vec wi, *velx, *velz, *rho, *rt, *exner;
-    PetscViewer viewer;
 
     PetscInitialize(&argc, &argv, (char*)0, help);
 
@@ -145,6 +191,8 @@ int main(int argc, char** argv) {
     pe   = new PrimEqns(topo, geom, dt);
     pe->step = startStep;
 
+    n2 = topo->nElsX*topo->nElsX;
+
     // initialise the z coordinate layer heights
     geom->initTopog(f_topog, z_from_eta);
 
@@ -152,15 +200,15 @@ int main(int argc, char** argv) {
     rho   = new Vec[NK];
     rt    = new Vec[NK];
     exner = new Vec[NK];
-    velz  = new Vec[topo->nElsX*topo->nElsX];
+    velz  = new Vec[n2];
     for(ki = 0; ki < NK; ki++) {
         VecCreateMPI(MPI_COMM_WORLD, topo->n1l, topo->nDofs1G, &velx[ki] );
         VecCreateMPI(MPI_COMM_WORLD, topo->n1l, topo->nDofs1G, &rho[ki]  );
         VecCreateMPI(MPI_COMM_WORLD, topo->n2l, topo->nDofs2G, &exner[ki]);
         VecCreateMPI(MPI_COMM_WORLD, topo->n2l, topo->nDofs2G, &rt[ki]);
     }
-    for(ii = 0; ii < topo->nElsX*topo->nElsX; ii++) {
-        VecCreateSeq(MPI_COMM_SELF, topo->nElsX*topo->nElsX, &velz[ii]);
+    for(ii = 0; ii < n2; ii++) {
+        VecCreateSeq(MPI_COMM_SELF, (NK-1)*topo->elOrd*topo->elOrd, &velz[ii]);
     }
 
     if(startStep == 0) {
@@ -171,41 +219,24 @@ int main(int argc, char** argv) {
             geom->write2(rho[ki],fieldname,0);
             sprintf(fieldname,"exner_%.3u", ki);
             geom->write2(exner[ki],fieldname,0);
-            sprintf(fieldname,"rt_%.3u", ki);
+            sprintf(fieldname,"rhoTheta_%.3u", ki);
             geom->write2(rt[ki],fieldname,0);
         }
-        for(ii = 0; ii < topo->nElsX*topo->nElsX; ii++) {
+        for(ii = 0; ii < n2; ii++) {
             sprintf(fieldname,"velocity_z_%.4u", ii);
             geom->write1(velz[ii],fieldname,0);
         }
     } else {
-        for(ki = 0; ki < NK; ki++) {
-            sprintf(fieldname, "output/density_%.3u_%.4u.vec", ki, startStep);
-            PetscViewerBinaryOpen(PETSC_COMM_WORLD, fieldname, FILE_MODE_READ, &viewer);
-            VecLoad(rho[ki], viewer);
-            PetscViewerDestroy(&viewer);
-
-            sprintf(fieldname, "output/velocity_x_%.3u_%.4u.vec", ki, startStep);
-            PetscViewerBinaryOpen(PETSC_COMM_WORLD, fieldname, FILE_MODE_READ, &viewer);
-            VecLoad(velx[ki], viewer);
-            PetscViewerDestroy(&viewer);
-
-            sprintf(fieldname, "output/exner_%.3u_%.4u.vec", ki, startStep);
-            PetscViewerBinaryOpen(PETSC_COMM_WORLD, fieldname, FILE_MODE_READ, &viewer);
-            VecLoad(exner[ki], viewer);
-            PetscViewerDestroy(&viewer);
-
-            sprintf(fieldname, "output/rt_%.3u_%.4u.vec", ki, startStep);
-            PetscViewerBinaryOpen(PETSC_COMM_WORLD, fieldname, FILE_MODE_READ, &viewer);
-            VecLoad(rt[ki], viewer);
-            PetscViewerDestroy(&viewer);
-        }
-        for(ii = 0; ii < topo->nElsX*topo->nElsX; ii++) {
-            sprintf(fieldname, "output/velocity_z_%.4u_%.4u.vec", ki, startStep);
-            PetscViewerBinaryOpen(PETSC_COMM_WORLD, fieldname, FILE_MODE_READ, &viewer);
-            VecLoad(velz[ii], viewer);
-            PetscViewerDestroy(&viewer);
-        }
+        sprintf(fieldname,"density");
+        LoadVecs(rho  , NK, fieldname, startStep);
+        sprintf(fieldname,"velocity_x");
+        LoadVecs(velx , NK, fieldname, startStep);
+        sprintf(fieldname,"exner");
+        LoadVecs(exner, NK, fieldname, startStep);
+        sprintf(fieldname,"rhoTheta");
+        LoadVecs(rt   , NK, fieldname, startStep);
+        sprintf(fieldname,"velociyt_z");
+        LoadVecs(velz , n2, fieldname, startStep);
     }
 
     vort_0 = mass_0 = ener_0 = 0.0;
@@ -239,6 +270,21 @@ int main(int argc, char** argv) {
                                                  << "\t" << (ener_n-ener_0)/ener_0 << endl;
                 file.close();
             }
+
+            for(ki = 0; ki < NK; ki++) {
+                sprintf(fieldname,"velocity_x_%.3u", ki);
+                geom->write1(velx[ki],fieldname,step/dumpEvery);
+                sprintf(fieldname,"density_%.3u", ki);
+                geom->write2(rho[ki],fieldname,step/dumpEvery);
+                sprintf(fieldname,"exner_%.3u", ki);
+                geom->write2(exner[ki],fieldname,step/dumpEvery);
+                sprintf(fieldname,"rhoTheta_%.3u", ki);
+                geom->write2(rt[ki],fieldname,step/dumpEvery);
+            }
+            for(ii = 0; ii < n2; ii++) {
+                sprintf(fieldname,"velocity_z_%.4u", ii);
+                geom->write1(velz[ii],fieldname,step/dumpEvery);
+            }
         }
     }
 
@@ -253,7 +299,7 @@ int main(int argc, char** argv) {
         VecDestroy(&rt[ki]   );
         VecDestroy(&exner[ki]);
     }
-    for(ii = 0; ii < topo->nElsX*topo->nElsX; ii++) {
+    for(ii = 0; ii < n2; ii++) {
         VecDestroy(&velz[ki] );
     }
     delete[] velx;
