883187570b8703dcf2d46294e7a747264ba45354
semuser <semuser@vic-17-172.vpn.csiro.au>
2018-08-23 09:46:28 +1000

    dumping every 30 mins

--- cc_topo/pes/UMJS14.cpp
+++ cc_topo/pes/UMJS14.cpp
@@ -256,7 +256,7 @@ int main(int argc, char** argv) {
     int startStep = atoi(argv[1]);
     double dt = 60.0;
     int nSteps = 48*60;
-    int dumpEvery = 20;
+    int dumpEvery = 30;
     ofstream file;
     Topo* topo;
     Geom* geom;
