161dd8509ccb007d247c720a8744087c09e2479b
semuser <semuser@vic-17-184.vpn.csiro.au>
2018-09-19 17:38:55 +1000

    Using 60s time step for 24 3rd order els per edge, and dumping every 3 hours

--- cc_topo/pes/UMJS14.cpp
+++ cc_topo/pes/UMJS14.cpp
@@ -266,9 +266,9 @@ int main(int argc, char** argv) {
     char fieldname[50];
     bool dump;
     int startStep = atoi(argv[1]);
-    double dt = 30.0;
+    double dt = 60.0;
     int nSteps = 8*24*120;
-    int dumpEvery = 240;
+    int dumpEvery = 180;
     ofstream file;
     Topo* topo;
     Geom* geom;
