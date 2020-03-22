eee91dbf4fb45deb1eabec55907fb080930e2ea2
semuser <semuser@aquila.local>
2020-03-22 21:54:16 +1100

    adding a class to do the vertical viscosity of the horizontal velocity as a 3D implicit solve

---
+++ sandbox/eul/Solve3D.cpp
@@ -0,0 +1,214 @@
+#include <iostream>
+
+#include <petsc.h>
+#include <petscis.h>
+#include <petscvec.h>
+#include <petscmat.h>
+#include <petscksp.h>
+
+#include "LinAlg.h"
+#include "Basis.h"
+#include "Topo.h"
+#include "Geom.h"
+#include "L2Vecs.h"
+#include "ElMats.h"
+#include "VertOps.h"
+#include "Assembly.h"
+#include "Solve3D.h"
+
+#define SCALE 1.0e+8
+
+using namespace std;
+
+Solve3D::Solve3D(Topo* _topo, Geom* _geom, double dt, double del2) {
+    int lSize = _geom->nk * _topo->n1l;
+    int gSize = _geom->nk * _topo->nDofs1G;
+    int index;
+    int *inds_g;
+    int mi, mf, nCols, row_g, col_g[999];
+    const int* cols;
+    const double *vals;
+    double val_g[999], dzInv;
+    int nlocal, first_local, size;
+    PC pc, subpc;
+    KSP* subksp;
+    Vec v_l, v_g;
+    IS is_l, is_g;
+    GaussLobatto* quad = new GaussLobatto(_topo->elOrd);
+    LagrangeNode* node = new LagrangeNode(_topo->elOrd, quad);
+    LagrangeEdge* edge = new LagrangeEdge(_topo->elOrd, node);
+    Umat* M1 = new Umat(topo, geom, node, edge);
+
+    topo = _topo;
+    geom = _geom;
+
+    elOrd = _topo->elOrd;
+    nElsX = _topo->nElsX;
+    elOrd2 = elOrd*elOrd;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    VecCreateSeq(MPI_COMM_SELF, lSize, &vl);
+    VecCreateMPI(MPI_COMM_WORLD, lSize, gSize, &x);
+    VecCreateMPI(MPI_COMM_WORLD, lSize, gSize, &b);
+
+    // create the scatter
+    index = 0;
+    inds_g = new int[lSize];
+    for(int ei = 0; ei < topo->nElsX*topo->nElsX; ei++) {
+        for(int kk = 0; kk < geom->nk; kk++) {
+            for(int ii = 0; ii < elOrd2; ii++) {
+                inds_g[index++] = rank*(geom->nk*topo->n2l) + ei*geom->nk*elOrd2 + kk*elOrd2 + ii;
+            }
+        }
+    }
+    
+    VecCreateSeq(MPI_COMM_SELF, lSize, &v_l);
+    VecCreateMPI(MPI_COMM_WORLD, lSize, gSize, &v_g);
+    ISCreateStride(MPI_COMM_SELF, lSize, 0, 1, &is_l);
+    ISCreateGeneral(MPI_COMM_WORLD, lSize, inds_g, PETSC_COPY_VALUES, &is_g);
+
+    VecScatterCreate(v_g, is_g, v_l, is_l, &scat);
+    delete[] inds_g;
+
+    VecDestroy(&v_l);
+    VecDestroy(&v_g);
+    ISDestroy(&is_l);
+    ISDestroy(&is_g);
+
+    MatCreate(MPI_COMM_WORLD, &M);
+    MatSetSizes(M, lSize, lSize, gSize, gSize);
+    MatSetType(M, MATMPIAIJ);
+    MatMPIAIJSetPreallocation(M, 3*2*2*(elOrd)*(elOrd+1), PETSC_NULL, 3*2*2*(elOrd)*(elOrd+1), PETSC_NULL);
+    MatZeroEntries(M);
+
+    // assemble the matrix
+    M1->assemble(0, SCALE, false);
+    MatGetOwnershipRange(M1->M, &mi, &mf);
+    for(int kk = 0; kk < geom->nk; kk++) {
+        if(kk > 0) {
+            dzInv = (1.0/geom->thick[kk-1][0]/geom->thick[kk-1][0])/(0.5*(geom->thick[kk-1][0] + geom->thick[kk+0][0]));
+            for(int mm = mi; mm < mf; mm++) {
+                MatGetRow(M1->M, mm, &nCols, &cols, &vals);
+                row_g = kk * topo->nDofs1G + mm;
+                for(int col_i = 0; col_i < nCols; col_i++) {
+                    col_g[col_i] = (kk-1) * topo->nDofs1G + cols[col_i];
+                    val_g[col_i] = dt * del2 * dzInv * vals[col_i];
+                }
+                MatSetValues(M, 1, &row_g, nCols, col_g, val_g, ADD_VALUES);
+                MatRestoreRow(M1->M, mm, &nCols, &cols, &vals);
+            }
+
+            dzInv  = (1.0/geom->thick[kk][0]/geom->thick[kk][0])/(0.5*(geom->thick[kk-1][0] + geom->thick[kk+0][0]));
+            for(int mm = mi; mm < mf; mm++) {
+                MatGetRow(M1->M, mm, &nCols, &cols, &vals);
+                row_g = kk * topo->nDofs1G + mm;
+                for(int col_i = 0; col_i < nCols; col_i++) {
+                    col_g[col_i] = kk * topo->nDofs1G + cols[col_i];
+                    val_g[col_i] = -dt * del2 * dzInv * vals[col_i];
+                }
+                MatSetValues(M, 1, &row_g, nCols, col_g, val_g, ADD_VALUES);
+                MatRestoreRow(M1->M, mm, &nCols, &cols, &vals);
+            }
+        }
+
+        dzInv  = (1.0/geom->thick[kk][0]);
+        for(int mm = mi; mm < mf; mm++) {
+            MatGetRow(M1->M, mm, &nCols, &cols, &vals);
+            row_g = kk * topo->nDofs1G + mm;
+            for(int col_i = 0; col_i < nCols; col_i++) {
+                col_g[col_i] = kk * topo->nDofs1G + cols[col_i];
+                val_g[col_i] = dzInv * vals[col_i];
+            }
+            MatSetValues(M, 1, &row_g, nCols, col_g, val_g, ADD_VALUES);
+            MatRestoreRow(M1->M, mm, &nCols, &cols, &vals);
+        }
+
+        if(kk < geom->nk-1) {
+            dzInv  = (1.0/geom->thick[kk][0]/geom->thick[kk][0])/(0.5*(geom->thick[kk+1][0] + geom->thick[kk+0][0]));
+            for(int mm = mi; mm < mf; mm++) {
+                MatGetRow(M1->M, mm, &nCols, &cols, &vals);
+                row_g = kk * topo->nDofs1G + mm;
+                for(int col_i = 0; col_i < nCols; col_i++) {
+                    col_g[col_i] = kk * topo->nDofs1G + cols[col_i];
+                    val_g[col_i] = -dt * del2 * dzInv * vals[col_i];
+                }
+                MatSetValues(M, 1, &row_g, nCols, col_g, val_g, ADD_VALUES);
+                MatRestoreRow(M1->M, mm, &nCols, &cols, &vals);
+            }
+
+            dzInv = (1.0/geom->thick[kk+1][0]/geom->thick[kk+1][0])/(0.5*(geom->thick[kk+1][0] + geom->thick[kk+0][0]));
+            for(int mm = mi; mm < mf; mm++) {
+                MatGetRow(M1->M, mm, &nCols, &cols, &vals);
+                row_g = kk * topo->nDofs1G + mm;
+                for(int col_i = 0; col_i < nCols; col_i++) {
+                    col_g[col_i] = (kk+1) * topo->nDofs1G + cols[col_i];
+                    val_g[col_i] = dt * del2 * dzInv * vals[col_i];
+                }
+                MatSetValues(M, 1, &row_g, nCols, col_g, val_g, ADD_VALUES);
+                MatRestoreRow(M1->M, mm, &nCols, &cols, &vals);
+            }
+        }
+    }
+    MatAssemblyBegin(M, MAT_FINAL_ASSEMBLY);
+    MatAssemblyEnd(  M, MAT_FINAL_ASSEMBLY);
+
+    KSPCreate(MPI_COMM_WORLD, &ksp);
+    KSPSetOperators(ksp, M, M);
+
+    KSPGetPC(ksp, &pc);
+    KSPSetType(ksp, KSPGMRES);
+    PCSetType(pc, PCBJACOBI);
+    PCBJacobiSetTotalBlocks(pc, size*topo->nElsX*topo->nElsX, NULL);
+    KSPSetUp(ksp);
+    PCBJacobiGetSubKSP(pc, &nlocal, &first_local, &subksp);
+
+    for(int ii = 0; ii < nlocal; ii++) {
+        KSPGetPC(subksp[ii], &subpc);
+        PCSetType(subpc, PCLU);
+    }
+    KSPSetTolerances(ksp, 1.0e-16, 1.0e-50, PETSC_DEFAULT, 1000);
+    KSPSetOptionsPrefix(ksp, "ksp_schur_");
+    KSPSetFromOptions(ksp);
+
+    delete M1;
+    delete quad;
+    delete node;
+    delete edge;
+}
+
+void Solve3D::Solve(Vec* bg, Vec* xg) {
+    RepackVector(bg, b);
+    KSPSolve(ksp, b, x);
+    UnpackVector(x, xg);
+}
+
+Solve3D::~Solve3D() {
+    VecDestroy(&vl);
+    VecDestroy(&x);
+    VecDestroy(&b);
+    VecScatterDestroy(&scat);
+    MatDestroy(&M);
+    KSPDestroy(&ksp);
+}
+
+void Solve3D::RepackVector(Vec* ux, Vec _u) {
+    int ind_g;
+    int lShift = rank*(geom->nk*topo->n1l + (geom->nk-1)*topo->n2l);
+    PetscScalar *uxArray, *uArray;
+
+    VecGetArray(_u, &uArray);
+    for(int kk = 0; kk < geom->nk; kk++) {
+        VecGetArray(ux[kk], &uxArray);
+        for(int ii = 0; ii < topo->n1l; ii++) {
+            ind_g = lShift + kk*(topo->n1l + topo->n2l) + ii;
+            uArray[ind_g] = uxArray[ii];
+        }
+        VecRestoreArray(ux[kk], &uxArray);
+    }
+    VecRestoreArray(_u, &uArray);
+}
+
+void Solve3D::UnpackVector(Vec _u, Vec* ux) {
+}
---
+++ sandbox/eul/Solve3D.h
@@ -0,0 +1,24 @@
+class Solve3D {
+    public:
+        Solve3D(Topo* _topo, Geom* _geom, double dt, double del2);
+        ~Solve3D();
+        int rank;
+        Topo* topo;
+        Geom* geom;
+        Vec vl;
+        Vec x;
+        Vec b;
+        Mat M;
+        KSP ksp;
+        VecScatter scat;
+        void RepackVector(Vec* vx, Vec v);
+        void UnpackVector(Vec v, Vec* vx);
+        void Solve(Vec* xg, Vec* bg);
+
+    private:
+        int elOrd;
+        int nElsX;
+        int elOrd2;
+        int* elInds2_l(int ex, int ey);
+        int* elInds2_g(int ex, int ey);
+};
